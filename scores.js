c2ced8c55e57ef86be82f978578b975c01353000
philmatarese <122230448+philmatarese@users.noreply.github.com>
2023-01-11 06:18:15 -0500

    Update scores.js

--- scores.js
+++ scores.js
@@ -3,7 +3,7 @@ const loadScoreBoard = async (scoreboardId) => {
  
  const scoreUrl = `https://keepthescore.co/api/${scoreboardId}/board/`;
  const loadingDiv = document.querySelector('div.loading');
- if (loadingDiv) loadingDiv.firstChild.data = `Loading scores from #{scoreUrl}...`;
+ if (loadingDiv) loadingDiv.firstChild.data = `Loading scores from ${scoreUrl}...`;
 
  const response = await fetch(scoreUrl);
  const tableData = await response.json();
